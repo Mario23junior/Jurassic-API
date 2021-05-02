a62ea4b4e25b727afc173d8456f24e7207deb3f4
Mario23junior <mariojunior3251@gmail.com>
2021-05-02 03:52:25 -0300

    Salvando as informacaos cientificas no banco de dados

--- src/main/java/com/project/jurassic/Controller/ClassificacaoCientificaController.java
+++ src/main/java/com/project/jurassic/Controller/ClassificacaoCientificaController.java
@@ -1,10 +1,29 @@
 package com.project.jurassic.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.jurassic.Model.ClassificacaoCientifica;
+import com.project.jurassic.Repository.RepositoryClassificacaoCientifica;
 
 @RequestMapping("/v1/api/classificacaoCientifica")
 @RestController
 public class ClassificacaoCientificaController {
+   
+	private RepositoryClassificacaoCientifica repositoryClassificacaoCientifica;
 
+	
+	public ClassificacaoCientificaController(RepositoryClassificacaoCientifica repositoryClassificacaoCientifica) {
+		this.repositoryClassificacaoCientifica = repositoryClassificacaoCientifica;
+ 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ClassificacaoCientifica save(@RequestBody ClassificacaoCientifica classificacaoCientifica ) {
+		 return repositoryClassificacaoCientifica.save(classificacaoCientifica);
+	}
 }
