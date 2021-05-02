901b8cf352c3d3ad8051d2eafc7774184322f54b
Mario23junior <mariojunior3251@gmail.com>
2021-05-02 08:01:58 -0300

    Deletando informacaoe cientifica do dinossauro por id

--- src/main/java/com/project/jurassic/Controller/ClassificacaoCientificaController.java
+++ src/main/java/com/project/jurassic/Controller/ClassificacaoCientificaController.java
@@ -3,6 +3,7 @@ package com.project.jurassic.Controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,10 +41,22 @@ public class ClassificacaoCientificaController {
 		                       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"id não encontrado"));
 	}
 	
-
 	@GetMapping("/familia")
 		public  List<ClassificacaoCientifica> listByFamilia(@RequestParam(name = "familia") String familia) {
 		return repositoryClassificacaoCientifica.findByFamiliaIgnoreCaseContaining(familia);	
 	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		  repositoryClassificacaoCientifica
+		                      .findById(id)
+		                      .map(delete -> {
+		                    	  repositoryClassificacaoCientifica.delete(delete);
+		                    	  return delete;
+		                      }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Nenhuma informação encontrada para ser deletada"));
+	}
+	
+	
 
 }
