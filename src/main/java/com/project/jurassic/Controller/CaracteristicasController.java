30451d915f452841d4074ee0a2fa9400c975854a
Mario23junior <mariojunior3251@gmail.com>
2021-05-03 01:49:17 -0300

    listando as caracteristicas por id

--- src/main/java/com/project/jurassic/Controller/CaracteristicasController.java
+++ src/main/java/com/project/jurassic/Controller/CaracteristicasController.java
@@ -1,11 +1,16 @@
 package com.project.jurassic.Controller;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.jurassic.Model.Caracteristicas;
 import com.project.jurassic.Repository.RepositoryCaracteristicas;
@@ -24,6 +29,14 @@ public class CaracteristicasController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Caracteristicas save(@RequestBody Caracteristicas caracteristicas) {
 		return repositoryCaracteristicas.save(caracteristicas);
+	}
+	
+	@GetMapping("/{id}")
+ 	public Optional<Caracteristicas> listById(@PathVariable Long id) {
+		      Optional<Caracteristicas> list = Optional.of(repositoryCaracteristicas
+		    		                        .findById(id)
+		    		                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Nenhum id foi encontrado")));
+		return list;
 	}
 	
 }
