d839543997fd0d5ff17ad0f3ecdf2cd4b5d658de
Mario23junior <mariojunior3251@gmail.com>
2021-05-03 02:12:25 -0300

    Listando as caracteristicas por tipo de alimento consumido

--- src/main/java/com/project/jurassic/Repository/RepositoryCaracteristicas.java
+++ src/main/java/com/project/jurassic/Repository/RepositoryCaracteristicas.java
@@ -1,5 +1,7 @@
 package com.project.jurassic.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.project.jurassic.Model.Caracteristicas;
 @Repository
 public interface RepositoryCaracteristicas extends JpaRepository<Caracteristicas, Long> {
 
+     List<Caracteristicas> findByAlimentoIgnoreCaseContaining(String alimento);
 }
